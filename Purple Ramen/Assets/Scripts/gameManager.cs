f79734e0d89e8456ab436ec5fb19f6e0378b33b9
Sylas Braveheart-Kern <145995602+EnviousOrb@users.noreply.github.com>
2024-03-10 17:49:24 -0400

    Win Condition added

---
+++ Purple Ramen/Assets/Scripts/gameManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] GameObject menuWin;
+
+    public GameObject player;
+    public playerController PS;
+    public bool isPaused;
+    float TimeScaleOrig;
+    int enemyCount;
+
+    // Awake is called before Start
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        PS = player.GetComponent<playerController>();
+        TimeScaleOrig = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+                statePaused();
+            else
+                stateResume();
+        }
+        if (enemyCount == 0)
+        {
+            stateWin();
+        }
+    }
+    public void stateWin()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        menuActive = menuWin;
+        menuActive.SetActive(true);
+    }
+    public void statePaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        menuActive = menuPause;
+        menuActive.SetActive(true);
+    }
+
+    public void stateResume()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = TimeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+    public void UpdateEnemyCount(int amount)
+    {
+        enemyCount += amount;
+    }
+}
