d0e2c5b8bdee0169620b26ce191de90aadb4c823
Sylas Braveheart-Kern <145995602+EnviousOrb@users.noreply.github.com>
2024-03-10 15:54:07 -0400

    Going through and commenting code
    Added comments for the Bullet and CameraController

---
+++ Purple Ramen/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    public static Game_Manager instance;
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+
+    public GameObject player;
+    //public PlayerController PS;
+    public bool isPaused;
+    float TimeScaleOrig;
+
+    // Awake is called before Start
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        //PS = player.GetComponent<Player_Controller>();
+        TimeScaleOrig = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel") && menuActive == null)
+        {
+            statePaused();
+            menuActive = menuPause;
+            menuActive.SetActive(isPaused);
+        }
+    }
+
+    public void statePaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateResume()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = TimeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
+    }
+}
