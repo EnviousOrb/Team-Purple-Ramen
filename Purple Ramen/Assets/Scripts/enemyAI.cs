ef7b78ded83111f98a36dc53b4ce15f8cd70e067
Johnathan <82104706+thecharmingbard@users.noreply.github.com>
2024-03-10 15:26:18 -0400

    More updates to enemyAI script
    Added updates to the script based on day two information we learned.

--- Purple Ramen/Assets/Scripts/enemyAI.cs
+++ Purple Ramen/Assets/Scripts/enemyAI.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class enemyAI : MonoBehaviour, IDamage
 {
-    [SerializeField] int HP;
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform shootPos;
+
+    [SerializeField] int HP;
+    [SerializeField] int speed;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+    bool isShooting;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager.instance.updateGameGoal(1);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.SetDestination(gameManager.instance.player.transform.position);
 
+        if (!isShooting)
+        {
+            StartCoroutine(shoot());
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
     }
 
     public void takeDamage(int amount)
@@ -26,6 +51,7 @@ public class enemyAI : MonoBehaviour, IDamage
         // Event game object dead.
         if (HP <= 0)
         {
+            gameManager.instance.updateGameGoal(-1);
             Destroy(gameObject);
         }
     }
@@ -33,7 +59,7 @@ public class enemyAI : MonoBehaviour, IDamage
     IEnumerator flashRed()
     {
         model.material.color = Color.red;
-        yield return new WaitForSeconds(1.1f);
+        yield return new WaitForSeconds(0.1f);
         model.material.color = Color.white;
     }
 }
