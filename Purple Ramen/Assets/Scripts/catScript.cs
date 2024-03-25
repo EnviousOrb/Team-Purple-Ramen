2670f33419827ecfcb0fed6c8607fb316c613ac5
Johnathan <82104706+thecharmingbard@users.noreply.github.com>
2024-03-24 23:50:25 -0400

    NPC cat takes the quest item and you get a win

--- Purple Ramen/Assets/Scripts/catScript.cs
+++ Purple Ramen/Assets/Scripts/catScript.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class catScript : MonoBehaviour
 {
+    [SerializeField] private ItemData requiredItem; // The item the cat wants
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            gameManager.instance.catMenu.SetActive(true);
+            playerController player = other.GetComponent<playerController>();
+            if (player != null && player.itemList.Contains(requiredItem))
+            {
+                // Player has the required item. Trigger the win condition.
+                gameManager.instance.stateWin();
+
+                // Optionally, remove the item from the player's inventory
+                player.itemList.Remove(requiredItem);
+
+                // Update the inventory UI to reflect this change
+                UIManager.instance.UpdateInventoryUI(player.itemList);
+            }
+            else
+            {
+                // Player doesn't have the required item. Just show the regular cat menu.
+                gameManager.instance.catMenu.SetActive(true);
+            }
         }
     }
 
-
     private void OnTriggerExit(Collider other)
     {
-        gameManager.instance.catMenu.SetActive(false);
+        if (other.CompareTag("Player"))
+        {
+            // You might want to deactivate the win menu here as well if you're using OnTriggerExit to close the catMenu.
+            gameManager.instance.catMenu.SetActive(false);
+        }
     }
 }
+
