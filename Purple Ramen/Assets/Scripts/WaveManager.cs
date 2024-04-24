d47dd47b03ba7728a4831062eba44275aae76ef7
Jaidon White <jaidon82@gmail.com>
2024-04-24 12:00:27 -0400

    Wave Manager works
    Wave Manager is fully functional

--- Purple Ramen/Assets/Scripts/WaveManager.cs
+++ Purple Ramen/Assets/Scripts/WaveManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -8,6 +10,7 @@ public class WaveManager : MonoBehaviour
     public GameObject[] enemies;
     public GameObject[] spawnLocs;
     public GameObject miniBoss;
+    private int enemiesRemaining;
     public float timeBetweenWaves = 10f;
     public float spawnRate = 1.0f;
 
@@ -19,7 +22,8 @@ public class WaveManager : MonoBehaviour
 
      void Start()
     {
-        startWave();        
+        startWave();  
+        
     }
 
 
@@ -27,6 +31,7 @@ public class WaveManager : MonoBehaviour
     {
         waveNumber++;
         StartCoroutine(spawnEnemies() );
+        enemiesRemaining = waveNumber * 2;
 
     }
 
@@ -48,7 +53,7 @@ public class WaveManager : MonoBehaviour
         }
 
        
-
+       
        
         if(waveNumber<5 &&waveInProgress==false)
         {
@@ -57,4 +62,25 @@ public class WaveManager : MonoBehaviour
         else if(waveNumber>=5 &&waveInProgress==false) 
             Instantiate(miniBoss, spawnLocs[Random.Range(0, spawnLocs.Length)].transform.position, spawnLocs[Random.Range(0,spawnLocs.Length)].transform.rotation );
     }
+
+    public void enemyDefeated()
+    {
+        if(!waveInProgress)
+        {
+            return;
+
+        }
+
+        enemiesRemaining--;
+
+        if(enemiesRemaining == 0)
+        {
+            waveInProgress=false;
+            Debug.Log("Wave " +  waveNumber + " Completed!");
+            waveNumber++;
+            StartCoroutine(spawnEnemies());
+        }
+
+    }
+
 }
